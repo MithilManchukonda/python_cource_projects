61ab145f047fd6ed45f3bbc2aaab39e4c58471c6
MithilManchukonda <mithilmanchukonda@gmail.com>
2025-11-24 20:56:54 +0530

    Update todo_list_project.py
    add case _:
    in this code

--- todo_list_project.py
+++ todo_list_project.py
@@ -5,26 +5,28 @@ while True:
     user_action = user_action.strip()
 
     match user_action:
-        case 'add':
+        case "add":
             todo = input("Enter a todo: ")
             todos.append(todo)
 
-        case 'show':
+        case "show":
             for index, item in enumerate(todos):
-                row = f"{index + 1}-{item}"
+                row = f"{index + 1} - {item}"
                 print(row)
 
-        case 'edit':
+        case "edit":
             number = int(input("Number of the todo to edit: "))
             number = number - 1
             new_todo = input("Enter new todo: ")
             todos[number] = new_todo
 
-        case 'complete':
+        case "complete":
             number = int(input("Number of the todo to complete: "))
             todos.pop(number - 1)
 
-        case 'exit':
+        case "exit":
+            print("Bye!")
             break
 
-print("Bye!")
+        case _:
+            print("Invalid command")
